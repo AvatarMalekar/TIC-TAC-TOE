074d0430a39c7eec15abc9481a6d7d21414947fd
Your Name <avatarmalekar4@gmail.com>
2020-02-01 15:42:00 +0530

    ADDED:feature to reset Game Board

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -1,2 +1,18 @@
 #!/bin/bash -x
 echo "---------------------------------------------:WELLCOME TO TIC TAC TOE:--------------------------------------------------------"
+NUMBER_OF_ROWS=3
+NUMBER_OF_COLUMNS=3
+
+declare -a gameBoard
+
+function boardReset(){
+	for(( i=0; i<$NUMBER_OF_ROWS; i++ ))
+	do
+		for(( j=0; j<$NUMBER_OF_COLUMNS; j++ ))
+		do
+			gameBoard[$i,$j]="."	
+		done
+	done
+}
+
+boardReset
