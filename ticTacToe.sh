3968c5d706697f099a43ff7f31d12edd8ce2545d
Your Name <avatarmalekar4@gmail.com>
2020-02-06 15:11:09 +0530

    REFACTORED:smart computer function

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -1,12 +1,13 @@
+
 #!/bin/bash
 echo "---------------------------------------------:WELLCOME TO TIC TAC TOE:--------------------------------------------------------"
 
 #CONSTANT
-NUMBER_OF_ROWS=3
-NUMBER_OF_COLUMNS=3
-PLAYER=0
-X=0
-O=1
+readonly NUMBER_OF_ROWS=3
+readonly NUMBER_OF_COLUMNS=3
+readonly PLAYER=0
+readonly X=0
+readonly O=1
 
 #VARIABLES
 tieCounter=0
@@ -60,64 +61,76 @@ function gameBoardDisplay(){
 	done
 }
 
+function displayWinner(){
+	local strValue=$1
+	if [[ $strValue == $"XXX" ]]
+	then
+		if [[ $"X" == $userLetter ]]
+		then
+			echo "YOU WIN.."
+		else
+			echo "Computer Wins"
+		fi
+		exit 0
+	elif [[ $strValue == $"OOO" ]]
+	then
+		if [[ $"O" == $userLetter ]]
+		then
+			echo "YOU WIN.."
+		else
+			echo "Computer Wins.."
+		fi
+		exit 0
+	fi
+}
+
 function checkWinner(){
-	local str=""
+	local str
 	local curentStatus
 	((tieCounter++))
-	if [ $tieCounter -gt 4 ] #4 IS MINIMUM MOVES TO CHECK WINNER
+	if [ $tieCounter -gt 4 ] #4 IS MINIMUM MOVES TO WIN
 	then
-		for (( i=0; i<8; i++ ))
+		for (( i=0; i<$NUMBER_OF_ROWS; i++ ))
 		do
-			case $i in
-				0)
-					str="${gameBoard[0,0]}${gameBoard[0,1]}${gameBoard[0,2]}"
-					;;
-				1)
-					str="${gameBoard[1,0]}${gameBoard[1,1]}${gameBoard[1,2]}"
-					;;
-				2)
-					str="${gameBoard[2,0]}${gameBoard[2,1]}${gameBoard[2,2]}"
-					;;
-				3)
-					str="${gameBoard[0,0]}${gameBoard[1,0]}${gameBoard[2,0]}"
-					;;
-				4)
-					str="${gameBoard[0,1]}${gameBoard[1,1]}${gameBoard[2,1]}"
-					;;
-				5)
-					str="${gameBoard[0,2]}${gameBoard[1,2]}${gameBoard[2,2]}"
-					;;
-				6)
-					str="${gameBoard[0,0]}${gameBoard[1,1]}${gameBoard[2,2]}"
-					;;
-				7)
-					str="${gameBoard[2,0]}${gameBoard[1,1]}${gameBoard[0,2]}"
-					;;
-				*)
-					echo "Invalid entry"
-					;;
-			esac
-			if [[ $str == $"XXX" ]]
-			then
-				if [[ $"X" == $userLetter ]]
-				then
-					echo "YOU WIN.."
-				else
-					echo "Computer Wins"
-				fi
-				exit 0
-			elif [[ $str == $"OOO" ]]
-			then
-				if [[ $"O" == $userLetter ]]
-				then
-					echo "YOU WIN.."
-				else
-					echo "Computer Wins.."
-				fi
-				exit 0
-			fi
+			str=""
+			for (( j=0; j<$NUMBER_OF_COLUMNS; j++ ))
+			do
+				str="$str${gameBoard[$i,$j]}"
+			done
+			displayWinner $str
 		done
-		if [ $tieCounter -gt 8 ] #8 IS TIE CONDITION
+		for (( i=0; i<$NUMBER_OF_ROWS; i++ ))
+		do
+			str=""
+			for (( j=0; j<$NUMBER_OF_COLUMNS; j++ ))
+			do
+				str="$str${gameBoard[$j,$i]}"
+			done
+			displayWinner $str
+		done
+		str=""
+		for (( i=0; i<$NUMBER_OF_ROWS; i++ ))
+		do
+			for (( j=0; j<$NUMBER_OF_COLUMNS; j++ ))
+			do
+				if [ $i -eq $j ]
+				then
+					str="$str${gameBoard[$i,$j]}"
+				fi
+			done
+		done
+		displayWinner $str
+		str=""
+		for (( i=0; i<$NUMBER_OF_ROWS; i++ ))
+		do
+			for (( j=$((2-$i)); j<$NUMBER_OF_COLUMNS; j++ ))
+			do
+				str="$str${gameBoard[$i,$j]}"
+				break;
+			done
+		done
+		displayWinner $str
+		if [ $tieCounter -gt 8 ] # 8 IS NUMBER OF BLOCKS IN TIC-TAC-TOE
 		then
 			echo "Its a Tie..Both Played Well.."
 			exit 0
@@ -140,23 +153,20 @@ function getComputerLetter(){
 }
 
 function takeCorners(){
-	retFlag=1
+	retFlag=0
 	local fillLetter=$1
-	if [[ ${gameBoard[0,0]} == $"." ]]
-	then
-		gameBoard[0,0]=$fillLetter
-	elif [[ ${gameBoard[2,2]} == $"." ]]
-	then
-		gameBoard[2,2]=$fillLetter
-	elif [[ ${gameBoard[2,0]} == $"." ]]
-	then
-		gameBoard[2,0]=$fillLetter
-	elif [[ ${gameBoard[0,2]} == $"." ]]
-	then
-		gameBoard[0,2]=$fillLetter
-	else
-		retflag=0
-	fi
+	for (( i=0; i<$NUMBER_OF_ROWS; i=$(($i+2)) ))
+	do
+		for (( j=0; j<$NUMBER_OF_COLUMNS; j=$(($j+2)) ))
+		do
+			if [[ ${gameBoard[$i,$j]} == $"." ]]
+			then
+				gameBoard[$i,$j]=$fillLetter
+				retFlag=1
+				return
+			fi
+		done
+	done
 }
 
 function takeCenter(){
@@ -166,109 +176,141 @@ function takeCenter(){
 	then
 		gameBoard[1,1]=$fillLetter
 	else
-		retflag=0
+		retFlag=0
 	fi
 }
 
 function takeSides(){
+	retFlag=0
+	local fillLetter=$1
+	for (( i=0; i<$(($NUMBER_OF_ROWS-1)); i++ ))
+	do
+		for (( j=0; j<$(($NUMBER_OF_COLUMNS-1)); j++ ))
+		do
+			if [ $(($i%2)) -eq 0 ]
+			then
+				if [[ ${gameBoard[$i,$(($i+1))]} == $"." ]]
+				then
+					gameBoard[$i,$(($i+1))]=$fillLetter
+					retFlag=1
+					return
+				fi
+			else
+				if [[ ${gameBoard[$(($i+1)),$i]} == $"." ]]
+				then
+					gameBoard[$(($i+1)),$i]=$fillLetter
+					retFlag=1
+					return
+				fi
+			fi
+		done
+	done
+}
+
+function initialize(){
+	x=0
+	y=0
+	counter1=0
+	counter2=0
+}
+
+function insertValue(){
+	local p=$1
+	local q=$2
+	local setLetter=$3
+	gameBoard[$p,$q]=$setLetter
 	retFlag=1
-	if [[ ${gameBoard[1,0]} == $"." ]]
+	flag2=1
+}
+
+function setValue(){
+	local p=$1
+	local q=$2
+	local letterToMatch=$3
+	if [[ ${gameBoard[$p,$q]} == $letterToMatch ]]
 	then
-		gameBoard[1,0]=$fillLetter
-	elif [[ ${gameBoard[0,1]} == $"." ]]
+		((counter1++))
+	fi
+	if [[ ${gameBoard[$p,$q]} == $"." ]]
 	then
-		gameBoard[0,1]=$fillLetter
-	elif [[ ${gameBoard[1,2]} == $"." ]]
-	then
-		gameBoard[1,2]=$fillLetter
-	elif [[ ${gameBoard[2,1]} == $"." ]]
-	then
-		gameBoard[2,1]=$fillLetter
-	else
-		retflag=0
+		x=$p
+		y=$q
+		((counter2++))
 	fi
 }
 
 function checkBoard(){
 	local fillLetter=$1
 	local checkLetter=$2
-	retFlag=1
-	if [[ ${gameBoard[0,0]} == $checkLetter && ${gameBoard[0,1]} == $checkLetter && ${gameBoard[0,2]} == $"." ]]
+	retFlag=0
+	flag2=0
+	if [ $flag2 -eq 0 ]
 	then
-		gameBoard[0,2]=$fillLetter
-	elif [[ ${gameBoard[1,0]} == $checkLetter && ${gameBoard[1,1]} == $checkLetter && ${gameBoard[1,2]} == $"." ]]
-	then
-		gameBoard[1,2]=$fillLetter
-	elif [[ ${gameBoard[2,0]} == $checkLetter && ${gameBoard[2,1]} == $checkLetter && ${gameBoard[2,2]} == $"." ]]
-	then
-		gameBoard[2,2]=$fillLetter
-	elif [[ ${gameBoard[0,1]} == $checkLetter && ${gameBoard[0,2]} == $checkLetter && ${gameBoard[0,0]} == $"." ]]
-	then
-		gameBoard[0,0]=$fillLetter
-	elif [[ ${gameBoard[1,1]} == $checkLetter && ${gameBoard[1,2]} == $checkLetter && ${gameBoard[1,0]} == $"." ]]
-	then
-		gameBoard[1,0]=$fillLetter
-	elif [[ ${gameBoard[2,1]} == $checkLetter && ${gameBoard[2,2]} == $checkLetter && ${gameBoard[2,0]} == $"." ]]
-	then
-		gameBoard[2,0]=$fillLetter
-	elif [[ ${gameBoard[0,0]} == $checkLetter && ${gameBoard[1,0]} == $checkLetter && ${gameBoard[2,0]} == $"." ]]
-	then
-		gameBoard[2,0]=$fillLetter
-	elif [[ ${gameBoard[0,1]} == $checkLetter && ${gameBoard[1,1]} == $checkLetter && ${gameBoard[2,1]} == $"." ]]
-	then
-		gameBoard[2,1]=$fillLetter
-	elif [[ ${gameBoard[0,2]} == $checkLetter && ${gameBoard[1,2]} == $checkLetter && ${gameBoard[2,2]} == $"." ]]
-	then
-		gameBoard[2,2]=$fillLetter
-	elif [[ ${gameBoard[1,0]} == $checkLetter && ${gameBoard[2,0]} == $checkLetter && ${gameBoard[0,0]} == $"." ]]
-	then
-		gameBoard[0,0]=$fillLetter
-	elif [[ ${gameBoard[1,1]} == $checkLetter && ${gameBoard[2,1]} == $checkLetter && ${gameBoard[0,1]} == $"." ]]
-	then
-		gameBoard[0,1]=$fillLetter
-	elif [[ ${gameBoard[1,2]} == $checkLetter && ${gameBoard[2,2]} == $checkLetter && ${gameBoard[0,2]} == $"." ]]
-	then
-		gameBoard[0,2]=$fillLetter
-	elif [[ ${gameBoard[0,0]} == $checkLetter && ${gameBoard[1,1]} == $checkLetter && ${gameBoard[2,2]} == $"." ]]
-	then
-		gameBoard[2,2]=$fillLetter
-	elif [[ ${gameBoard[1,1]} == $checkLetter && ${gameBoard[2,2]} == $checkLetter && ${gameBoard[0,0]} == $"." ]]
-	then
-		gameBoard[0,0]=$fillLetter
-	elif [[ ${gameBoard[2,0]} == $checkLetter && ${gameBoard[1,1]} == $checkLetter && ${gameBoard[0,2]} == $"." ]]
-	then
-		gameBoard[0,2]=$fillLetter
-	elif [[ ${gameBoard[1,1]} == $checkLetter && ${gameBoard[0,2]} == $checkLetter && ${gameBoard[2,0]} == $"." ]]
-	then
-		gameBoard[2,0]=$fillLetter
-	elif [[ ${gameBoard[1,0]} == $checkLetter && ${gameBoard[1,2]} == $checkLetter && ${gameBoard[1,1]} == $"." ]]
-	then
-		gameBoard[1,1]=$fillLetter
-	elif [[ ${gameBoard[0,1]} == $checkLetter && ${gameBoard[2,1]} == $checkLetter && ${gameBoard[1,1]} == $"." ]]
-	then
-		gameBoard[1,1]=$fillLetter
-	elif [[ ${gameBoard[0,0]} == $checkLetter && ${gameBoard[2,2]} == $checkLetter && ${gameBoard[1,1]} == $"." ]]
-	then
-		gameBoard[1,1]=$fillLetter
-	elif [[ ${gameBoard[2,0]} == $checkLetter && ${gameBoard[0,2]} == $checkLetter && ${gameBoard[1,1]} == $"." ]]
-	then
-		gameBoard[1,1]=$fillLetter
-	elif [[ ${gameBoard[0,0]} == $checkLetter && ${gameBoard[0,2]} == $checkLetter && ${gameBoard[0,1]} == $"." ]]
-	then
-		gameBoard[0,1]=$fillLetter
-	elif [[ ${gameBoard[2,0]} == $checkLetter && ${gameBoard[2,2]} == $checkLetter && ${gameBoard[2,1]} == $"." ]]
-	then
-		gameBoard[2,1]=$fillLetter
-	elif [[ ${gameBoard[0,0]} == $checkLetter && ${gameBoard[2,0]} == $checkLetter && ${gameBoard[1,0]} == $"." ]]
-	then
-		gameBoard[1,0]=$fillLetter
-	elif [[ ${gameBoard[0,2]} == $checkLetter && ${gameBoard[2,2]} == $checkLetter && ${gameBoard[1,2]} == $"." ]]
-	then
-		gameBoard[1,2]=$fillLetter
-	else
-		retFlag=0
+	for (( i=0; i<$NUMBER_OF_ROWS; i++ ))
+	do
+		initialize
+		for (( j=0; j<$NUMBER_OF_COLUMNS; j++ ))
+		do
+			setValue $i $j $checkLetter
+		done
+		if [ $counter1 -eq 2 -a $counter2 -eq 1 ]
+		then
+			insertValue $x $y $fillLetter
+		fi
+	done
 	fi
 
+	if [ $flag2 -eq 0 ]
+	then
+	for (( i=0; i<$NUMBER_OF_ROWS; i++ ))
+	do
+		initialize
+		for (( j=0; j<$NUMBER_OF_COLUMNS; j++ ))
+		do
+			setValue $j $i $checkLetter
+		done
+		if [ $counter1 -eq 2 -a $counter2 -eq 1 ]
+		then
+			insertValue $x $y $fillLetter
+		fi
+	done
+	fi
+
+	if [ $flag2 -eq 0 ]
+	then
+	initialize
+	for (( i=0; i<$NUMBER_OF_ROWS; i++ ))
+	do
+		for (( j=0; j<$NUMBER_OF_COLUMNS; j++ ))
+		do
+			if [ $i -eq $j ]
+			then
+				setValue $i $j $checkLetter
+			fi
+		done
+		if [ $counter1 -eq 2 -a $counter2 -eq 1 ]
+		then
+			insertValue $x $y $fillLetter
+		fi
+	done
+	fi
+
+	if [ $flag2 -eq 0 ]
+	then
+	initialize
+	for (( i=0; i<$NUMBER_OF_ROWS; i++ ))
+	do
+		for (( j=$((2-$i)); j<$NUMBER_OF_COLUMNS; j++ ))
+		do
+			setValue $i $j $checkLetter
+			break;
+		done
+		if [ $counter1 -eq 2 -a $counter2 -eq 1 ]
+		then
+			insertValue $x $y $fillLetter
+		fi
+	done
+	fi
 }
 
 function smartComputer(){
@@ -318,7 +360,6 @@ function humanPlayer(){
 	smartComputer
 }
 
-#MAIN
 boardReset
 userLetter=$(getAssignedLetter)
 computerLetter=$(getComputerLetter $userLetter )
